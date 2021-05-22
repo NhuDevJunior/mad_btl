51201e7c7b7b0ed1f2abab6bb7f20f11d8994390
Ngoc <minhngocnguyenduy@gmail.com>
2021-05-22 22:22:54 +0700

    Thêm chức năng xem danh sách bài báo và đọc báo theo mô hình MVVM

---
+++ app/src/main/java/com/example/android/newsfeed/fragment/HighlightNewsFragment.java
@@ -0,0 +1,32 @@
+package com.example.android.newsfeed.fragment;
+
+import android.view.View;
+
+import com.example.android.newsfeed.adapter.MyNewsAdapter;
+import com.example.android.newsfeed.model.News;
+
+import java.util.List;
+
+public class HighlightNewsFragment extends BaseListNewsFragment {
+    @Override
+    protected List<News> getListNews() {
+        return newsViewModel.getListHighlightsNews().getValue();
+    }
+
+    @Override
+    protected void observeListNews() {
+        newsViewModel.getListHighlightsNews().observe(getActivity(), news -> {
+            if (news != null) {
+                newsAdapter.addAll(news);
+                binding.recyclerView.setAdapter(newsAdapter);
+                binding.loadingIndicator.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    @Override
+    protected void initiateRefresh() {
+        newsAdapter.clearAll();
+        newsViewModel.getListHighLightNewsFromApi();
+    }
+}
