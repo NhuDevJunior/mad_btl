c2f2e7cfc89374fd1fa9b89d0f0b847da969951e
Han Nhu <nhuhc@zent.vn>
2021-05-24 12:19:44 +0700

    I2

--- app/src/main/java/com/example/android/newsfeed/fragment/FootballVNNewsFragment.java
+++ app/src/main/java/com/example/android/newsfeed/fragment/FootballVNNewsFragment.java
@@ -1,32 +1,32 @@
 package com.example.android.newsfeed.fragment;
 
-import android.util.Log;
 import android.view.View;
 
-import com.example.android.newsfeed.adapter.MyNewsAdapter;
 import com.example.android.newsfeed.model.News;
+import com.example.android.newsfeed.utils.Constants;
 
 import java.util.List;
 
-public class HighlightNewsFragment extends BaseListNewsFragment {
+public class FootballVNNewsFragment extends BaseListNewsFragment{
     @Override
     protected List<News> getListNews() {
-        return newsViewModel.getListHighlightsNews().getValue();
+        return newsViewModel.getListNewsByCategoryFootballVN().getValue();
     }
 
     @Override
     protected void observeListNews() {
-        newsViewModel.getListHighlightsNews().observe(getViewLifecycleOwner(), news -> {
+        newsViewModel.getListNewsByCategoryFootballVN().observe(getViewLifecycleOwner(), news -> {
             if (news != null) {
                 newsAdapter.addAll(news);
                 binding.loadingIndicator.setVisibility(View.GONE);
             }
         });
+
     }
 
     @Override
     protected void initiateRefresh() {
         newsAdapter.clearAll();
-        newsViewModel.getListHighLightNewsFromApi();
+        newsViewModel.getNewsByCategoryFootballVN(Constants.CATEGORY_FOOTBALL_VN);
     }
 }
